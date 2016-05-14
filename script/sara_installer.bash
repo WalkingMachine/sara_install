f5eded95c5e7936900af93bcdf915f8bd7cb7449
Thierry Pouplier <thierrypouplier@gmail.com>
2016-05-14 12:10:18 -0400

    Add installer script

--- script/sara_installer.bash
+++ script/sara_installer.bash
@@ -1,26 +1,42 @@
 #!/bin/sh
 
 # Adding source and key
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 
 # To get the latest package lists
-apt-get update
-apt-get upgrade
-
-# Install git pip
-apt-get install git pip -y
+sudo apt-get update
+sudo apt-get upgrade
 
 # ROS installation
-apt-get install -y ros-indigo-desktop-full -y
+sudo apt-get install -y ros-indigo-desktop-full -y
+
+# Install tools
+sudo apt-get install git pip python-wstool -y
 
 # Package installation
-apt-get install ros-indigo-openni2-launch ros-indigo-urg-node ros-indigo-xsens-driver \
+sudo apt-get install ros-indigo-openni2-launch ros-indigo-urg-node ros-indigo-xsens-driver \
 ros-indigo-joystick-drivers ros-indigo-navigation ros-indigo-pocketshinx ros-indigo-rosserial \
 ros-indigo-roboteq-driver ros-indigo-roboteq-diagnostics ros-indigo-roboteq-msgs ros-indigo-smach \
 ros-indigo-rtabmap-ros ros-indigo-gazebo-ros ros-indigo-slam-gmapping ros-indigo-map-laser \
 ros-indigo-cob-perception-common ros-indigo-moveit-full ros-indigo-geographic-info \
-ros-indigo-zbar-ros ros-indigo-dynamixel-motor couchdb -y
+ros-indigo-zbar-ros ros-indigo-dynamixel-motor couchdb python-rosinstall -y
 
 # Interface grafic couchapp
 pip install -U couchapp
+
+# rosdep
+sudo rosdep init
+rosdep update
+
+# workspace creation
+mkdir -p ~/sara_ws
+cd ~/sara_ws
+wstool init src ~/sara_install/rosinstall/sara_full.rosinstall
+wstool update -t src
+
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bahsrc
+
+rosdep update
+
