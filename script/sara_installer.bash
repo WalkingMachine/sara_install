9e71c4291bb918e16431d47a196954d42151bb6c
salco <JeSuisSalco@gmail.com>
2016-09-20 13:06:39 -0400

    Revert "Make some protection"

--- script/sara_installer.bash
+++ script/sara_installer.bash
@@ -1,44 +1,33 @@
-#!/bin/bash
-
-# Check to be sure the user is root
-if [[ $EUID  -ne 0 ]]
-  then
-  echo "Please run as root"
-  exit
-fi
+#!/bin/sh
 
 echo "S.A.R.A. Installation beginning"
 
 # Adding source and key
 echo "Adding source and key"
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
-if [ $? -ne 0 ]; then
-	echo "Adding source failed."
-	exit
-fi
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 
 # To get the latest package lists
 echo "Getting latest package lists and upgrading system"
 
-apt update -y
-apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 
 # ROS installation
 echo "Installing ROS"
 
-apt install -y ros-indigo-desktop-full -y
+sudo apt install -y ros-indigo-desktop-full -y
 
 # Install tools
 echo "Installing tools"
 
-apt install git python-pip python-wstool -y
+sudo apt install git python-pip python-wstool -y
 
 # Package installation
 echo "Installing package"
 
-apt install ros-indigo-openni2-launch ros-indigo-urg-node ros-indigo-xsens-driver \
+sudo apt install ros-indigo-openni2-launch ros-indigo-urg-node ros-indigo-xsens-driver \
 ros-indigo-joystick-drivers ros-indigo-navigation ros-indigo-pocketsphinx ros-indigo-rosserial \
 ros-indigo-roboteq-diagnostics ros-indigo-roboteq-msgs ros-indigo-smach \
 ros-indigo-rtabmap-ros ros-indigo-gazebo-ros ros-indigo-slam-gmapping ros-indigo-map-laser \
@@ -49,18 +38,17 @@ ros-indigo-soem espeak ros-indigo-object-recognition-core  ros-indigo-object-rec
 # Utility installation
 echo "Installing Utility"
 
-apt install htop lm-sensors tig
-
+sudo apt install htop lm-sensors tig
 
 # Interface graphic couchapp
 echo "Installing graphic for couchapp"
 
-pip install -U couchapp
+sudo pip install -U couchapp
 
 # pyttsx
 echo "Installing pyttsx"
 
-pip install pyttsx
+sudo pip install pyttsx
 
 # workspace creation
 echo "Creating Workspace"
@@ -77,14 +65,9 @@ source ~/.bashrc
 # rosdep
 echo "ros dep"
 
-rosdep init
+sudo rosdep init
 rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro indigo -y
-if [ $? -ne 0 ]; then
-        echo "ros dep failed."
-        exit
-fi
-
 
 # catkin_make to build all the packages
 echo "Building packages"
@@ -99,10 +82,6 @@ catkin_make
 
 #enable database online viewer
 rosrun object_recognition_core push.sh
-if [ $? -eq 0 ]; then
-	chown -R $USER ~/sara_ws
-	echo "Continue with remote_setup.md"
-	echo "Installation done go get a beer and code"
-else
-	echo "The Installation faild. Unable to use command: rosrun"
-fi
+
+echo "Continue with remote_setup.md"
+echo "Installation done go get a beer and code"
