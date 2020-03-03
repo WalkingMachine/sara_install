b038680387f2837cb5b8b0f27b9b06f6d0714684
raphael-duchaine <duchainer@gmail.com>
2020-03-03 15:00:33 -0500

    Make "safe_rm" safer

--- script/clear_workspace.sh
+++ script/clear_workspace.sh
@@ -8,6 +8,7 @@
 WSDIR="${$(readlink -f ${0%/*})//\/script/}"
 
 # Move to the workspace
+PREV_PWD=$(pwd)
 cd "$WSDIR"
 
 
@@ -15,9 +16,8 @@ cd "$WSDIR"
 ## Remove all packages
 
 mv src/.rosinstall ./
-# "safe" rm in 2 steps : first the content, then the empty folders
-mv build/ devel/ src/ ~/.local/share/Trash/ > /dev/null
-mv build/ devel/ src/ ~/.local/share/Trash/ > /dev/null
+# "safe" rm in 2 steps : first copy the content in the trash, then rm it
+cp -alf build devel src ~/.local/share/Trash/ > /dev/null && rm -rf build/ devel/ src/ > /dev/null
 
 mkdir src
 mv .rosinstall src
@@ -26,4 +26,4 @@ mv .rosinstall src
 ## End
 
 # Move back to the original position
-cd - > /dev/null
+cd $PREV_PWD > /dev/null
